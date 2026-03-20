d1bdfeee3d49279f95022637c1a7975f9a560926
mateverr <mattpittaya@gmail.com>
2026-03-20 04:47:47 +0000

    in class code

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+#include "BST.h"
+int main(int argc, char **argv) {
+  unsigned int i; // counter to loop from 1-10
+  BST b;
+
+  for (i = 1; i < argc; ++i) 
+  {
+       b.insert_node (atoi(argv[i])); // insert node according to amount of argc
+       cout << argv[i] << " inserted" << endl;
+  } // end for
+
+  // cout << "inOrder" << endl;
+
+  // need to finish preOrder and postOrder function
+
+  b.print(3);
+  // the line above runs
+  // now ma
+
+
+  //b.searchtree(11);
+ 
+} // end main
---
+++ makefile
@@ -0,0 +1,4 @@
+complie: main.cpp
+	g++ main.cpp -o BST_O
+run: BST
+	./BST
