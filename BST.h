deaf94ee3ac01acb0d9fd1c2e9a6ef6d841f58b0
mateverr <mattpittaya@gmail.com>
2026-03-21 15:08:52 +0000

    comment + clean

--- BST.h
+++ BST.h
@@ -5,77 +5,69 @@ class BST {
   int size; // tell how big the search tree is
   TreeNodePtr rootPtr;
 
-public:
-  BST();
-  ~BST();
-
-  void insert_node(int);
-  void print(int = 0);
+  public:
+    BST();
+    ~BST();
+    void insert_node(int);
+    void print(int = 0);
     /* 0= inOrder 1=preOrder 2=postorder 3=printeverything 4=3+ print tree*/
-
 };
 
 // Additional functions
-  void inOrder(TreeNodePtr);
-  void preOrder(TreeNodePtr);
-  void postOrder(TreeNodePtr);
-  void printTree(TreeNodePtr,int); //The real lab
-  void kill(TreeNodePtr);
+void inOrder(TreeNodePtr);
+void preOrder(TreeNodePtr);
+void postOrder(TreeNodePtr);
+void printTree(TreeNodePtr,int); //The real lab
+void kill(TreeNodePtr);
 
-void BST::insert_node(int value) // Finish insert_node function
-{
+void BST::insert_node(int value) {
+  // Finish insert_node function
   // Start with simple insertion
+  
   int inserted = 0;
   TreeNodePtr new_node, t;
   new_node = new TreeNode(value); // Create an empty node
                                   // Call a new node
                                   // Need to add new node to existing tree
-  if (new_node) // If create new node
-  {
-      if (size == 0)
+  // If create new node
+  if (new_node) {
+    if (size == 0) rootPtr = new_node;
+      
+    else {
+      t = rootPtr;
+      while (inserted == 0) // everytime the function is called inserted == 0
       {
-        rootPtr = new_node;
-      }
-      else 
-      {
-        t = rootPtr;
-        
-        while (inserted == 0) // everytime the function is called inserted == 0
-        {
-              //once the location is found --> inserted=1
-              //1.should it go left or right
-              //2. should it be inserted, if available inserted, if not move
-              //3. once inserted make insert=1
+        //once the location is found --> inserted=1
+        //1.should it go left or right
+        //2. should it be inserted, if available inserted, if not move
+        //3. once inserted make insert=1
 
-              if (t->get_value() >= value) 
-              {
-                // go to the left tree
-                if (t->move_left()) t=t->move_left(); // not free, just move
-                // Same as if ()
-                // so if the left side is not NULL, move 
-                else // Left side is NULL, available, insert
-                {
-                  t->set_left(new_node);
-                  inserted = 1; // Stop the search
-                }
-                
-              }
-              else 
-              {
-                // go right --> insert right away / move your t
-                if (t->move_right()) t=t->move_right();
+        if (t->get_value() >= value) {
+          // go to the left tree
+          if (t->move_left()) t=t->move_left(); // not free, just move
+          // Same as if ()
+          // so if the left side is not NULL, move 
+          
+          else { 
+            // Left side is NULL, available, insert
+            t->set_left(new_node);
+            inserted = 1; 
+            // Stop the search
+          }       
+        }
+              
+        else {
+          if (t->move_right()) t=t->move_right();
+          // go right --> insert right away / move your t
 
-                else
-                {
-                  t->set_right(new_node);
-                  inserted = 1; // Stop the search
-                }
-            
-              }
+          else {t->set_right(new_node); inserted = 1;}
+          // Stop the search                
         }
       }
+    }
 
-      ++size; // Increase the size
+    ++size; // Increase the size
+
    /*   if(First Node){
           
       }
@@ -97,8 +89,7 @@ void BST::insert_node(int value) // Finish insert_node function
 }
 
 
-void BST::print(int option) 
-{
+void BST::print(int option) {
   switch (option) {
   case 0:
     inOrder(rootPtr);
@@ -130,91 +121,56 @@ void BST::print(int option)
   }
 }
 
+BST::BST() { rootPtr=NULL; size=0; }
+BST::~BST() { cout << "Kill BST" << endl; }
 
-BST::BST() 
-{
-  rootPtr=NULL;
-  size=0;
-}
-
-BST::~BST() 
-{
-  cout << "Kill BST" << endl; 
-}
-
-void inOrder(TreeNodePtr treePtr) // Recursive
-// Go down each layer of root until the bottom
-{
+// Recursive
+void inOrder(TreeNodePtr treePtr) { 
+  // Go down each layer of root until the bottom
   // TreeNodePtr treePtr=rootPtr;
-  if (treePtr) 
-  {
+  if (treePtr) {
     // if tree is not empty, then traverse
     inOrder(treePtr->move_left()); // Recursion to the left
     cout << setw(3) << treePtr->get_value(); // print the value
     inOrder(treePtr->move_right()); // Recursion to the right
-  }                                 // end if
-} // end function
-  //printTree(rootPtr,0);
+  }                              
+} 
 
-void printTree(TreeNodePtr treePtr,int level = 0) 
-{
+void printTree(TreeNodePtr treePtr,int level = 0) {
   // TreeNodePtr treePtr=rootPtr;
-  if (treePtr == nullptr) 
-  {
-    return;
-  }
+  if (treePtr == nullptr) return;
 
   level++;
-  //treePtr = treePtr->move_right();
   printTree(treePtr->move_right(), level);
-
   cout << setw(3*level) << treePtr->get_value() << endl;
-  
   printTree(treePtr->move_left(), level);
   
-  //cout << setw(3*level) << treePtr->get_value() << endl;
-  
-  
-  // end if
-} // end function
+} 
 
 
-void postOrder(TreeNodePtr treePtr) 
-{
+void postOrder(TreeNodePtr treePtr) {
   // TreeNodePtr treePtr=rootPtr;
   if (treePtr == nullptr) return;
   
   postOrder(treePtr->move_left());
-
   postOrder(treePtr->move_right());
-
   cout << setw(3) << treePtr->get_value();
+} 
 
-
-  // end if
-} // end function
-
-void kill(TreeNodePtr treePtr) 
-{
+void kill(TreeNodePtr treePtr) {// Need work
   // TreeNodePtr 
   if(treePtr)
   {
-       //delete everything
-        
+       //delete everything 
   }                                          // end if
 } // end function
 
-void preOrder(TreeNodePtr treePtr) 
-{
-
+void preOrder(TreeNodePtr treePtr) {
   // TreeNodePtr treePtr=rootPtr;
   if (treePtr == nullptr) return; // no branch to traverse to
 
   cout << setw(3) << treePtr->get_value();
-
   preOrder(treePtr->move_left());
-
-  preOrder(treePtr->move_right());
-                                     
-} // end function
+  preOrder(treePtr->move_right());                                     
+} 
 
--- main.cpp
+++ main.cpp
@@ -3,26 +3,16 @@
 using namespace std;
 #include "BST.h"
 
-int main(int argc, char **argv) 
-{
+int main(int argc, char **argv) {
   unsigned int i; // counter to loop from 1-10
   BST b;
 
-  for (i = 1; i < argc; ++i) 
-  {
-       b.insert_node (atoi(argv[i])); // insert node according to amount of argc
+  for (i = 1; i < argc; ++i) {
+       b.insert_node (atoi(argv[i])); 
+       // insert node according to amount of argc
        cout << argv[i] << " inserted" << endl;
-  } // end for
-
-  // cout << "inOrder" << endl;
-
-  // need to finish preOrder and postOrder function
+  } 
 
   b.print(3);
-  // the line above runs
-  // now ma
-
-
   //b.searchtree(11);
- 
-} // end main
+} 
